dcc34dd7f23afbedcaec41a26c54f7c2c8b79c3e
diogo <diogo@debiandesktop.highsec.local>
2022-11-02 21:40:56 +0000

    amended config

--- config.fish
+++ config.fish
@@ -1,14 +1,3 @@
-#export FZF_DEFAULT_OPTS="--layout=reverse --inline-info"
-#export GO111MODULE=on
-#set -x GOROOT /usr/local/go
-#set -Ux EDITOR nvim
-#set -x GOPATH $HOME/go
-#set -e fish_user_paths
-#set -U fish_user_paths $HOME/.local/bin $HOME/.cargo/bin $GOROOT/bin $GOPATH/bin $fish_user_paths
-
-set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
-source /usr/share/powerline/bindings/fish/powerline-setup.fish
-powerline-setup
 alias backup='cd /mnt/backup-drive'
 alias data='cd /mnt/data-drive'
 alias home='cd ~/'
