7775d44be9a87604f6a4ab91124fd8980281f195
Diogo <dmvfa90@gmail.com>
2024-01-07 17:35:27 +0000

    fish config

--- config.fish
+++ config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Alias
+
+### IPTABLES ###
+#
+alias iptl='sudo iptables -L'
+alias iptlv='sudo iptables -L -n -v --line-numbers'
+
